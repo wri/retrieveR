160cb305a3b0231edc74764aa7fe5d6ac510d65f
John Brandt <john.brandt@yale.edu>
2018-07-31 14:29:29 -0400

    remove 10 lines of code

---
+++ R/download_embeddings.R
@@ -0,0 +1,10 @@
+#' Download neural embeddings pre-trained on 1,000+ environmental policy documents from 40 nations and 50+ NGOs.
+#' @param path Location to save the downloaded embeddings to, defaults to "/embeddings.bin"
+#' @keywords creation
+#' @export
+#' @examples
+#' download_embeddings()
+
+download_embeddings <- function(path = "embeddings.bin") {
+	download.file("https://github.com/wri/retrieveR/raw/master/data-raw/embeddings.bin", destfile = path)
+}
