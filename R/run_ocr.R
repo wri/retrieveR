8adb76748e26d5e66fbac286f7e05aca54e1a5cb
John Brandt <john.brandt@yale.edu>
2018-08-08 14:26:24 -0400

    OCR working on windows

--- R/run_ocr.R
+++ R/run_ocr.R
@@ -4,33 +4,28 @@
 #' @examples
 #' run_ocr()
 
-#run_ocr <- function() {
-#  download.file("https://raw.githubusercontent.com/wri/retrieveR/master/data-raw/ocr.py", destfile = "ocr.py")
-#  reticulate::source_python("ocr.py")
-#  file.remove("ocr.py")
-#}
-
-#run_ocr <- function(file) {
-#  pages <- pdftools::pdf_info(file)$pages
-#  dir.create(gsub("[.]pdf", "", file))
-#  dir <- gsub("[.]pdf", "", file)
-#  for(i in c(1:pages)) {
-#    pdftools::pdf_convert(file, format="tiff", dpi=250, page = i, filenames=paste0(dir, "/", as.character(i), ".tiff"), verbose=F)
-#    output <- rtika::tika_text(paste0(dir, "/", as.character(i), ".tiff"))
-#    write.table(output, paste0(dir, "/", as.character(i), ".txt"), row.names=F, col.names=F, quote=F)
-#  }
-#}
-
-
-run_ocr <- function(file) {
-  dir.create(gsub("[.]pdf", "", file))
-  dir <- gsub("[.]pdf", "", file)
-  tabulizer::split_pdf(file, outdir=dir)
-  pages <- length(list.files(dir))
-  print(pages)
-  for(i in c(1:pages)) {
-    print(paste0(dir, "/", dir, as.character(i), ".pdf"))
-    output <- rtika::tika_text(paste0(dir, "/", dir, as.character(i), ".pdf"))
-    write.table(output, paste0(dir, "/", as.character(i), ".txt"), row.names=F, col.names=F, quote=F)
+run_ocr <- function(path) {
+  files <- list.files(path)
+  files <- files[grepl("pdf", files)]
+  split_files <- function(file) {
+    dir <- gsub("[.]pdf", "", file)
+    dir.create(paste0(path, "/", dir))
+    file <- paste0(path, "/", file)
+    tabulizer::split_pdf(file, outdir=paste0(path, "/", dir))
+    gc()
+    cat("Converting", file, "\n")
   }
+  ocr <- function(name) {
+    pages <- list.files(name)
+    dir <- gsub("[0-9][.]pdf", "", name)
+    for(i in c(1:length(pages))) {
+      output <- rtika::tika_text(paste0(name, "/", pages[i]))
+      file.remove(paste0(name, "/", pages[i]))
+      write.table(output, paste0(name, "/", as.character(i), ".txt"), row.names=F, col.names=F, quote=F)
+    }
+  }
+  l <- lapply(files, split_files)
+  results <- list.dirs(path)
+  results <- results[grepl("/", results)]
+  l <- lapply(results, ocr)
 }
