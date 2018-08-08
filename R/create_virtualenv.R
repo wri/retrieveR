9c61c11fde9030098cee805754ceba59b871118b
John Brandt <john.brandt@yale.edu>
2018-08-08 10:29:00 -0400

    proper virtual environment for windows

--- R/create_virtualenv.R
+++ R/create_virtualenv.R
@@ -5,9 +5,19 @@
 #' @examples
 #' create_virtualenv()
 
-create_virtualenv <- function(name = "RetrieveR") {
-  reticulate::virtualenv_create(name)
-  reticulate::virtualenv_install(name, "PyPDF2")
-  reticulate::virtualenv_install(name, "tika")
-  reticulate::use_virtualenv(name)
+create_virtualenv <- function(name = "RetrieveR", type="conda") {
+  if(type == "virtual") {
+    reticulate::virtualenv_create(name)
+    reticulate::virtualenv_install(name, "PyPDF2")
+    reticulate::virtualenv_install(name, "tika")
+    reticulate::use_virtualenv(name)
+  }
+  else if(type == "conda") {
+    if(!name %in% conda_list()$name) {
+      reticulate::conda_create(name)
+      reticulate::conda_install(name, "PyPDF2")
+      reticulate::conda_install(name, "tika")
+      reticulate::use_condaenv(name)
+    }
+  }
 }
