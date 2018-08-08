28f8d1b80e0c124babb5bcbc1efd22f4a19a8c57
John Brandt <john.brandt@yale.edu>
2018-08-08 10:58:26 -0400

    testing R-native OCR

--- R/run_ocr.R
+++ R/run_ocr.R
@@ -4,8 +4,19 @@
 #' @examples
 #' run_ocr()
 
-run_ocr <- function() {
-  download.file("https://raw.githubusercontent.com/wri/retrieveR/master/data-raw/ocr.py", destfile = "ocr.py")
-  reticulate::source_python("ocr.py")
-  file.remove("ocr.py")
+#run_ocr <- function() {
+#  download.file("https://raw.githubusercontent.com/wri/retrieveR/master/data-raw/ocr.py", destfile = "ocr.py")
+#  reticulate::source_python("ocr.py")
+#  file.remove("ocr.py")
+#}
+
+run_ocr <- function(file) {
+  pages <- pdftools::pdf_info(file)$pages
+  dir.create(gsub("[.]pdf", "", file))
+  dir <- gsub("[.]pdf", "", file)
+  for(i in c(1:pages)) {
+    pdftools::pdf_convert(file, format="png", dpi=250, page = i, filenames=paste0(dir, "/", as.character(i), ".png"), verbose=F)
+    output <- rtika::tika_text(paste0(dir, "/", as.character(i), ".png"))
+    write.table(output, paste0(dir, "/", as.character(i), ".txt"), row.names=F, col.names=F, quote=F)
+  }
 }
