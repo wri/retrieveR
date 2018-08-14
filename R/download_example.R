0506d437d104267c4d88284cda9500bab686009e
John Brandt <john.brandt@yale.edu>
2018-08-14 10:41:37 -0400

    OCR is ~50x faster, add download_example

---
+++ R/download_example.R
@@ -0,0 +1,25 @@
+#' Downloads example PDFs for use with the vignette
+#' @keywords creation
+#' @export
+#' @examples
+#' download_example()
+
+download_example <- function() {
+  dir.create("pdfs")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Collective%20action%20for%20poverty%20reduction.pdf",
+                destfile="pdfs/Collective action for poverty reduction.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Collective%20action%20for%20sustainable%20development.pdf",
+                destfile="pdfs/Collective action for sustainable development.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Guidlines%20on%20land%20policy%20in%20africa.pdf",
+                destfile = "pdfs/Guidelines on land policy in africa.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Markets%20to%20leverage%20investment%20in%20restoration.pdf",
+                destfile = "pdfs/Markets to leverage investment in restoration.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Network%20approach%20to%20social%20capacity.pdf",
+                destfile = "pdfs/Network approach to social capacity.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Political%20economy%20of%20reforestation.pdf",
+                destfile = "pdfs/Political economy of reforestation.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Public-private%20partnership%20for%20restoration.pdf",
+                destfile = "pdfs/Public-private partnership for restoration.pdf")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Scaling%20biomass%20monitoring.pdf",
+                destfile = "pdfs/Scaling biomass monitoring.pdf")
+}
--- R/run_ocr.R
+++ R/run_ocr.R
@@ -11,24 +11,36 @@ run_ocr <- function(path) {
   files <- list.files(path)
   files <- files[grepl("pdf", files)]
   split_files <- function(file) {
+    id <- which(files == file)
+    setTxtProgressBar(pb, id)
     dir <- gsub("[.]pdf", "", file)
     dir.create(paste0(path, "/", dir))
     file <- paste0(path, "/", file)
     tabulizer::split_pdf(file, outdir=paste0(path, "/", dir))
     gc()
-    cat("Converting", file, "\n")
   }
   ocr <- function(name) {
+    id <- which(results == name)
+    setTxtProgressBar(pb, id)
     pages <- list.files(name)
+    pages <- paste0(name, "/", pages)
     dir <- gsub("[0-9][.]pdf", "", name)
-    for(i in c(1:length(pages))) {
-      output <- rtika::tika_text(paste0(name, "/", pages[i]))
-      file.remove(paste0(name, "/", pages[i]))
-      write.table(output, paste0(name, "/", as.character(i), ".txt"), row.names=F, col.names=F, quote=F)
+    item <- rtika::tika_text(pages)
+    for(i in c(1:length(item))) {
+      file.remove(pages[i])
+      write.table(item[i], paste0(name, "/", as.character(i), ".txt"), row.names=F,
+                  col.names=F, quote=F)
     }
   }
+
+  cat("Splitting pdf files \n")
+  pb <- txtProgressBar(min=0, max=length(files), style=3)
   l <- lapply(files, split_files)
+  close(pb)
   results <- list.dirs(path)
   results <- results[grepl("/", results)]
+  cat("Running OCR on the corpus \n")
+  pb <- txtProgressBar(min=0, max=length(results), style=3)
   l <- lapply(results, ocr)
+  close(pb)
 }
