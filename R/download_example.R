085993873c2e4a7171809253556683ed46641121
John Brandt <john.brandt@yale.edu>
2018-08-14 13:52:31 -0400

    download in binary mode on windows

--- R/download_example.R
+++ R/download_example.R
@@ -7,19 +7,19 @@
 download_example <- function() {
   dir.create("pdfs")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Collective%20action%20for%20poverty%20reduction.pdf",
-                destfile="pdfs/Collective action for poverty reduction.pdf")
+                destfile="pdfs/Collective action for poverty reduction.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Collective%20action%20for%20sustainable%20development.pdf",
-                destfile="pdfs/Collective action for sustainable development.pdf")
+                destfile="pdfs/Collective action for sustainable development.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Guidlines%20on%20land%20policy%20in%20africa.pdf",
-                destfile = "pdfs/Guidelines on land policy in africa.pdf")
+                destfile = "pdfs/Guidelines on land policy in africa.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Markets%20to%20leverage%20investment%20in%20restoration.pdf",
-                destfile = "pdfs/Markets to leverage investment in restoration.pdf")
+                destfile = "pdfs/Markets to leverage investment in restoration.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Network%20approach%20to%20social%20capacity.pdf",
-                destfile = "pdfs/Network approach to social capacity.pdf")
+                destfile = "pdfs/Network approach to social capacity.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Political%20economy%20of%20reforestation.pdf",
-                destfile = "pdfs/Political economy of reforestation.pdf")
+                destfile = "pdfs/Political economy of reforestation.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Public-private%20partnership%20for%20restoration.pdf",
-                destfile = "pdfs/Public-private partnership for restoration.pdf")
+                destfile = "pdfs/Public-private partnership for restoration.pdf", mode="wb")
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/pdfs/Scaling%20biomass%20monitoring.pdf",
-                destfile = "pdfs/Scaling biomass monitoring.pdf")
+                destfile = "pdfs/Scaling biomass monitoring.pdf", mode="wb")
 }
