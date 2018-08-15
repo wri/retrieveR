e35e23d5f5c2d21aef3e8ea69308744c6d5c3c87
John Brandt <john.brandt@yale.edu>
2018-08-15 16:41:23 -0400

    add install windows

--- R/install_windows.R
+++ R/install_windows.R
@@ -7,7 +7,7 @@
 #' install_windows()
 
 
-install_mac <- function() {
+install_windows <- function() {
   download.file("https://github.com/wri/retrieveR/raw/master/data-raw/wordVectors_2.0.zip", destfile="wordVectors_2.0.zip", quiet=T, mode="wb")
   install.packages("wordVectors_2.0.zip", repos=NULL, type="binary")
   file.remove("wordVectors_2.0.zip")
