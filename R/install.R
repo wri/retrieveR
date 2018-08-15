8d3ff8f4d9312d3be85ba4d7a38e2ddc4a26ec28
John Brandt <john.brandt@yale.edu>
2018-08-15 16:14:40 -0400

    update install

--- R/install.R
+++ R/install.R
@@ -8,7 +8,7 @@
 
 
 install <- function() {
-  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/wordVectors_1.0.tgz", destfile="wordVectors_1.0.tgz", quiet=T, mode="wb")
-  install.packages("wordVectors_1.0.tgz", repos=NULL)
-  file.remove("wordVectors_1.0.tgz")
+  download.file("https://github.com/wri/retrieveR/raw/master/data-raw/wordVectors_1.0.tar.gz", destfile="wordVectors_1.0.tar.gz", quiet=T, mode="wb")
+  install.packages("wordVectors_1.0.tar.gz", repos=NULL, type="source")
+  file.remove("wordVectors_1.0.tar.gz")
 }
